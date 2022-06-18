aa9a7cc3d21d1907b9b4979a74489fd9f0a2433b
Baraa Mashaal <baraamashaal@gmail.com>
2022-06-18 08:20:16 +0200

    replacing chart static data with the live one

--- app/components/vue-components/performance-chart.vue
+++ app/components/vue-components/performance-chart.vue
@@ -32,43 +32,14 @@ export default {
   components: {
     VChart,
   },
-
-  data() {
-    return {
-      chartData: [
-        {
-          date_ms: 1641772800000,
-          performance: 0.2,
-        },
-        {
-          date_ms: 1641859200000,
-          performance: 0.33,
-        },
-        {
-          date_ms: 1641945600000,
-          performance: 0.53,
-        },
-        {
-          date_ms: 1642032000000,
-          performance: 0.31,
-        },
-        {
-          date_ms: 1642118400000,
-          performance: 0.65,
-        },
-        {
-          date_ms: 1642204800000,
-          performance: 0.88,
-        },
-        {
-          date_ms: 1642291200000,
-          performance: 0.07,
-        },
-      ],
-    };
+  mounted() {
+    this.$store.dispatch('getChartData')
   },
 
   computed: {
+    chartData(){
+      return this.$store.state.chartData;
+    },
     initOptions() {
       return {
         width: "auto",
