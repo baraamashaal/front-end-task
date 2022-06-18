d78716892a8531ed00189f12d93c478e5947b2ac
Baraa Mashaal <baraamashaal@gmail.com>
2022-06-19 01:16:29 +0200

    adding load more logic

--- app/pages/home-page/home-page.controller.js
+++ app/pages/home-page/home-page.controller.js
@@ -6,17 +6,28 @@ function homePageController(Employees, $stateParams, $state) {
   const homePageVm = this;
   homePageVm.employees = [];
   homePageVm.searchText = $stateParams.filter;
+  homePageVm.isLastPage = false;
+  homePageVm.isLoading = false;
+  homePageVm.currentPage = 0;
 
   homePageVm.onSearchTextChange = function (value) {
     $state.go('.', { filter: value }, { notify: false });
     homePageVm.searchText = value;
   };
-  activate();
 
-  function activate() {
-    Employees.getEmployees()
-      .then(({ data }) => {
-        homePageVm.employees = homePageVm.employees.concat(data.employees);
+  homePageVm.loadMore = () => {
+    homePageVm.isLoading = true;
+    Employees.loadMoreEmployees(homePageVm.currentPage + 1)
+      .then(({ employees, current_page: currentPage, pages }) => {
+        homePageVm.employees = homePageVm.employees.concat(employees);
+        homePageVm.currentPage = currentPage;
+
+        if (currentPage === pages) {
+          homePageVm.isLastPage = true;
+        }
+        homePageVm.isLoading = false;
       });
-  }
+  };
+
+  homePageVm.loadMore();
 }
